e8e34769a909aa5aeb37976deb2aa64687848a6b
yobie888 <4ggdr5pjkk@privaterelay.appleid.com>
2026-05-04 10:13:59 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,25 +1,9 @@
-const sqlite3 = require("sqlite3").verbose();
-
-const db = new sqlite3.Database("./data.db");
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./database.db');
 
 db.serialize(() => {
-    db.run(`
-        CREATE TABLE IF NOT EXISTS users (
-            id TEXT PRIMARY KEY,
-            name TEXT,
-            score INTEGER,
-            rank TEXT
-        )
-    `);
-
-    db.run(`
-        CREATE TABLE IF NOT EXISTS messages (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            userId TEXT,
-            question TEXT,
-            answer TEXT
-        )
-    `);
+    db.run("CREATE TABLE IF NOT EXISTS users (id TEXT PRIMARY KEY, name TEXT, score INTEGER, rank TEXT)");
+    db.run("CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, userId TEXT, question TEXT, answer TEXT)");
 });
 
 module.exports = db;
