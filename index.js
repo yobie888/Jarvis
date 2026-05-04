6d604743ca1f1d22bac3e6a41f928dc5df531245
yobie888 <4ggdr5pjkk@privaterelay.appleid.com>
2026-05-04 11:35:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 require("dotenv").config();
-console.log("BOOT START");
+
+console.log("BOOT STARTED");
+
+process.on("uncaughtException", (err) => {
+    console.log("UNCAUGHT EXCEPTION:");
+    console.log(err);
+});
+
+process.on("unhandledRejection", (err) => {
+    console.log("UNHANDLED REJECTION:");
+    console.log(err);
+});
 
 const { Client, GatewayIntentBits } = require("discord.js");
 
+console.log("DISCORD IMPORT OK");
+
+const db = require("./db");
+console.log("DB IMPORT OK");
+
+const KNOWLEDGE = require("./knowledge");
+console.log("KNOWLEDGE IMPORT OK");
+
 const client = new Client({
     intents: [
-        GatewayIntentBits.Guilds
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
     ]
 });
 
@@ -13,4 +34,5 @@ client.once("ready", () => {
     console.log("BOT ONLINE");
 });
 
+console.log("LOGIN START");
 client.login(process.env.DISCORD_TOKEN);
